a585d75260487eb410b0b19569eacd43572a21cd
vmarquar <valentin.marquart@gmail.com>
2017-05-30 17:29:16 +0200

    created new markdown template

--- RMD-Template/dataCleanup.R
+++ RMD-Template/dataCleanup.R
@@ -1,10 +1,15 @@
 # use this script to import io.R and clean up your data
 # carry out all anaylsis in our RMD Script
 
-if(!exists("xls_to_dataframe", mode="function")) source("/Users/Valentin/Library/Mobile Documents/3L68KQB4HG~com~readdle~CommonDocuments/Documents/0 Allgemeine Zusatzdokumente/Code Samples/Code Samples R/io.R")
+if(!exists("xls_to_dataframe", mode="function")) source("io.R")
 
-gwt <- xls_to_dataframe(path='Übung_Histogramm.xlsx',sheet=1,range=NULL,header=TRUE)
+gwt <- xls_to_dataframe(path='/Users/Valentin/Library/Mobile Documents/3L68KQB4HG~com~readdle~CommonDocuments/Documents/2 Semester/4 Geostatistik/Ü1 Histogram/Übung_Histogramm.xlsx',sheet=1,range=NULL,header=TRUE)
 gwt1 <- gwt[,1] # 1. Spalte Achtung hier als Tibble, bei read.csv2() import wäre gwt1 ein vector
 hist(gwt1)
 as.data.frame(gwt)
-PAK1 <- PAK[!is.na(PAK)]
+PAK1 <- gwt$PAK[!is.na(gwt$PAK)]
+
+PAK2 <- PAK1 * 1000
+# Save session to file
+print(getwd())
+save.image(file = 'cleandata.RData')
